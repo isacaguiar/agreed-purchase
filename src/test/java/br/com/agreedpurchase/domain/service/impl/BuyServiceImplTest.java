e3fd2cf996b8389e203537bdcf88090872310554
Isac Aguiar <isacaguiar@gmail.com>
2023-04-18 19:21:52 -0300

    Adjustment

--- src/test/java/br/com/agreedpurchase/domain/service/impl/BuyServiceImplTest.java
+++ src/test/java/br/com/agreedpurchase/domain/service/impl/BuyServiceImplTest.java
@@ -1,24 +1,13 @@
 package br.com.agreedpurchase.domain.service.impl;
 
-import static br.com.agreedpurchase.domain.utils.ConstantsUtils.DELIVERY;
-import static br.com.agreedpurchase.domain.utils.ConstantsUtils.INVALID;
 import static br.com.agreedpurchase.domain.utils.ConstantsUtils.PERCENT;
-import static java.math.RoundingMode.HALF_UP;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import br.com.agreedpurchase.domain.exception.BusinessException;
 import br.com.agreedpurchase.domain.model.Buy;
 import br.com.agreedpurchase.utils.BuilderUtils;
-import java.math.BigDecimal;
-import java.util.Map;
-import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -32,10 +21,6 @@ class BuyServiceImplTest {
 
   @Test
   void shouldSuccessWhenExecuteBuy() {
-    BigDecimal amount = new BigDecimal(100);
-    BigDecimal discount = new BigDecimal(20);
-
-    //when(persistencePort.buy(any())).thenReturn(getBuyEntityWithItems(amount, discount,PERCENT));
     Buy buy = buyService.buy(BuilderUtils.loadBuyWithMapPersonAndFee(PERCENT));
 
     assertNotNull(buy);
